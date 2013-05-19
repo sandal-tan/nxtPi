1e50769775ad014711047a2c4b9990aeba7dfac8
Ian Baldwin <iantbaldwin@mac.com>
2013-05-19 14:58:17 -0400

    Remote Scripts
    Added scripts for the GUI of Remote/FreeControl

---
+++ Remote/scan.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Create Symbolic link to scan for networks
+# For use with Remote.app (needs Root privileges)
+# Ian Baldwin, 5/19/13
+
+ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/sbin/airport
